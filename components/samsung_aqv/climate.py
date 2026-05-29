e5f544aee596fefc811657d0a3a44293dd5a2d04
André Patrício <andre.patricio.0@gmail.com>
2026-05-29 18:14:27 +0100

    fix: use current climate_ir API (climate_ir_schema, not CLIMATE_IR_SCHEMA)

--- components/samsung_aqv/climate.py
+++ components/samsung_aqv/climate.py
@@ -1,7 +1,5 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate_ir"]
 CODEOWNERS = ["@andredp"]
@@ -9,11 +7,8 @@ CODEOWNERS = ["@andredp"]
 samsung_aqv_ns = cg.esphome_ns.namespace("samsung_aqv")
 SamsungAqvClimate = samsung_aqv_ns.class_("SamsungAqvClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend(
-    {cv.GenerateID(): cv.declare_id(SamsungAqvClimate)}
-)
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(SamsungAqvClimate)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await climate_ir.register_climate_ir(var, config)
+    await climate_ir.new_climate_ir(config)
